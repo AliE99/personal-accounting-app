81a25edbbbcafe5e640a27a15243e5eb2792d7e2
ALI ELAHI <alielahi.1378@gmail.com>
2020-09-29 10:58:35 +0330

    Connected to the database and Insert index_router

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+require("./server/database/db");
 const app = express();
+
+// Import Routers
+const index_router = require("./server/app/routes/index");
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+
+// Routes
+app.use("/", index_router);
 
 // listen for requests
 app.listen(3000, () => {
---
+++ server/database/db.js
@@ -0,0 +1,16 @@
+// Configuring the database
+const dbConfig = require("../config/database_config");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+//Connecting to Database
+mongoose
+    .connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Successfully connected to the database");
+    })
+    .catch((err) => {
+        console.log("Could not connect to the database. Exiting now...", err);
+        process.exit();
+    });
