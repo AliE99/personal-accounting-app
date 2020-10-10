0bf88a066be5104f4069064cf304f4f6e849dd2f
ALI ELAHI <alielahi.1378@gmail.com>
2020-10-10 12:21:13 +0330

    Add Collapse detail for account

--- client/src/components/BankAccountComponent.vue
+++ client/src/components/BankAccountComponent.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="container mt-5">
-    <h3 class="mt-5">Accounts :</h3>
-    <b-table hover :items="this.accountData" head-variant="dark"
-             striped :fields="accountFields"
-             :sort-by.sync="sortAccountBy"
-             :sort-desc.sync="sortDesc"
-             sort-icon-left></b-table>
-    <hr>
+  <div>
+    <b-table :items="this.accountData" :fields="accountFields" striped responsive="sm">
+      <template v-slot:cell(show_details)="row">
+        <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+          {{ row.detailsShowing ? "Hide" : "Show" }} Details
+        </b-button>
+
+      </template>
+
+      <template v-slot:row-details="row">
+        <b-card>
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>بانک :</b></b-col>
+            <b-col>{{ row.item.bank_name }}</b-col>
+          </b-row>
+
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>شماره حساب :</b></b-col>
+            <b-col>{{ row.item.account_number }}</b-col>
+          </b-row>
+
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>مقدار :</b></b-col>
+            <b-col>{{ row.item.amount }}</b-col>
+          </b-row>
+
+          <b-row class="mb-2">
+            <b-col sm="3" class="text-sm-right"><b>واحد پول:</b></b-col>
+            <b-col>{{ row.item.currency }}</b-col>
+          </b-row>
+
+        </b-card>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -21,9 +47,8 @@ export default {
       sortDesc: false,
       accountFields: [
         {key: "bank_name", sortable: true},
-        {key: "account_number", sortable: true},
         {key: "amount", sortable: true},
-        {key: "currency", sortable: false},
+        "show_details",
       ],
     };
   },
